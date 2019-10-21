d1e1f0348b151a78e7b8d98f1792ba336f9fcec9
sadham <n10294805@qut.edu.au>
2019-10-21 14:45:58 +1000

    Updating Base Files

---
+++ BA/ba/auth.py
@@ -0,0 +1,158 @@
+from flask import Blueprint,render_template, redirect, url_for, request
+from .models import User,Item,Bid
+from .forms import RegestierForm
+from .forms import LoginForm
+from .forms import itemForm
+from .forms import searchForm
+import datetime
+from . import db
+from werkzeug.utils import secure_filename
+import os
+from . import create_app
+
+# the function to get the upload path so we can store it to the database
+def check_upload_file(form):
+          # get file data from form
+
+
+          fp = form.image.data
+          filename= fp.filename
+          # get the current path of the module file... store file relative to this path
+          BASE_PATH= os.path.dirname(__file__)
+          #uploadfilelocation – directory of this file/static/image
+          upload_path= os.path.join(BASE_PATH,'static/img', secure_filename(filename))
+          # store relative path in DB as image location in HTML is relative
+          db_upload_path= '/static/img/'+ secure_filename(filename)
+          # save the file and return the dbupload path
+          fp.save(upload_path)
+          return db_upload_path
+
+
+mainbp = Blueprint('main',__name__)
+@mainbp.route('/Search', methods = ['GET'])
+def Search():
+    search_form=searchForm()
+    if (search_form.validate_on_submit()):
+        print('Search Form Submitted')
+        #get username,password and email from the form
+        Price = search_form.price.data
+        Location = search_form.location.data
+        info = Item.query.filter_by(price=Price,location=Location).first()  
+        return redirect(url_for('main.index'))
+
+
+
+# homepage route
+@mainbp.route('/')
+def index():
+    search_form = searchForm()
+    tag_line='Budget Accomadation: Cheap Sharehouse For Broke You!'
+    room = Item.query.order_by(Item.id.desc()).limit(3).all()
+
+    return render_template('base.html', room = room, search_form=search_form,  tag_line=tag_line)
+
+#item form route
+@mainbp.route('/landlord')
+def post():
+    tag_line="I'm the landlord"
+    
+    aform = itemForm()
+
+    return render_template('index_reuse.html', tag_line=tag_line,
+                    #form=form, form2=form2, 
+                    aform=aform)
+
+#testing
+#@mainbp.route('/<id>') 
+#def show(id): 
+#  info = Item.query.filter_by(id=id).first()  
+#  return render_template('u.html', info=info)
+
+#information page route
+@mainbp.route('/sharehouse/<id>')
+def sharehousePage(id):
+    info = Item.query.filter_by(id=id).first()  
+    tag_line='Budget Accomadation: Cheap Sharehouse For Broke You!'
+    name = Item.query.filter_by(id=id).first()  
+    return render_template('content.html', tag_line=tag_line, info=info)
+
+#fetch item form and insert it to database
+@mainbp.route('/create', methods = ['GET','POST'])
+def create_item():
+  aform = itemForm()
+  if aform.validate_on_submit():
+    db_file_path=check_upload_file(aform)
+    print(db_file_path)
+
+        # a simple function: doesnot   handleerrorsin filetypesand  filenot  beinguploaded
+    
+    # if the form was successfully submitted
+    # access the values in the form data
+    print([aform.title.data,aform.description.data,aform.gas.data,aform.price.data,aform.water.data,aform.address.data,aform.gas.data,aform.mobile.data])
+    
+    #insert item into database
+    newitem = Item(id = datetime.datetime.now().isoformat(),#as you can see I used datetime as Primary Key
+                title=aform.title.data, 
+                description=aform.description.data,
+                image= db_file_path,
+                price= aform.price.data,
+                address=aform.address.data,
+                water = aform.water.data,
+                wifi = aform.wifi.data,
+                eletricity = aform.eletricity.data,
+                gas = aform.gas.data,
+                mobile = aform.mobile.data
+                #user_id = "working on it",
+                )
+    # add the object to the db session
+    db.session.add(newitem)
+    
+    # commit to the database
+    db.session.commit()
+    #flash('Successfully created new travel destination', 'success')
+    print('Successfully created new room info', 'success')
+    return redirect(url_for('main.index'))
+@mainbp.route('/reg')
+def reg():
+    registerform = RegestierForm()
+    return render_template('register.html',registerform = registerform)
+@mainbp.route('/register', methods = ['POST'])
+def register():
+    registerform = RegestierForm()
+    if registerform.validate_on_submit():
+        print('Register Form Submitted')
+        #get username,password and email from the form
+        username = registerform.user_name.data
+        pass_word = registerform.password.data
+        email = registerform.email.data
+
+        #create password hash
+        #password_hash = generate_password_hash(pass_word)
+        user1 = User(name=username,emailid=email,password_hash=pass_word)
+        db.session.add(user1)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+@mainbp.route('/login')
+def login():
+    login_form = LoginForm()
+    return render_template('login.html',login_form = login_form)
+@mainbp.route('/log', methods = ['GET','POST'])
+def log():
+    login_form=LoginForm()
+    error=None
+    if(login_form.validate_on_submit()):
+        username =login_form.user_name.data
+        pass_word =login_form.pass_word.data
+        u1 =User.query.filter_by(name = user_name).first()
+
+        if u1 is None:
+            error='Incorrect Username'
+        elif not check_password_hash(u1.check_password_hash,password):
+            error='Incorrect Password'
+        if error is None:
+            return redirect(url_for('main.index'))
+        else:
+            print(error)
+            flash(error)
+    return render_template('',login_form=login_form,heading='Login')
+
