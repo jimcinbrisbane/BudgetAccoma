83749c89c6fb8be0e2bfe746bfff7db545a17353
nhavilah <51215537+nhavilah@users.noreply.github.com>
2019-10-31 16:53:22 +1000

    Init commit

--- BudgetAccoma-master/BA/ba/__init__.py
+++ BudgetAccoma-master/BA/ba/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from .forms import searchForm
+import os
 #init database
 db=SQLAlchemy()
 
